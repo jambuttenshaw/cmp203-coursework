580e3926fedbf4b591802ff5e4479b9a3cdeddde
Jamie Buttenshaw <73542559+jambuttenshaw@users.noreply.github.com>
2021-11-22 16:13:19 +0000

    Fix lots of portal related stuff

--- GraphicsProgramming/GraphicsProgramming/src/Scenes/CourseworkScene.cpp
+++ GraphicsProgramming/GraphicsProgramming/src/Scenes/CourseworkScene.cpp
@@ -55,44 +55,52 @@ void CourseworkScene::OnRender()
 
 	// disable writing to frame buffer
 	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+	glDepthMask(GL_FALSE);
 	// enable & setup stencil test
 	glEnable(GL_STENCIL_TEST);
 	glStencilFunc(GL_ALWAYS, 1, 1);
 	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
-	// when drawing into stencil buffer we do not want to write to depth buffer
-	glDisable(GL_DEPTH_TEST);
 	// draw our stencil
 	RenderHelper::drawMesh(portalScreen);
 
-	// re-enable depth test
-	glEnable(GL_DEPTH_TEST);
 	// re-enable writing to frame buffer
 	glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+	glDepthMask(GL_TRUE);
 
 	// we do not want to change the stencil buffer from here
 	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
 
 
 	// render everything on one side of the portal
-	glStencilFunc(GL_GREATER, 1, 1);
+	glStencilFunc(GL_EQUAL, 1, 1);
 
 	skybox2->render(sceneCamera->getPosition());
 	{
 		Transformation t({ 0, 0.5f, -3.0f }, { 0.5f * rot, 1.2f * rot, 0.0f }, Vector3::one);
 		RenderHelper::drawUnitCube();
 	}
+	
 
-	// render everything on the other side of the portal
-	glStencilFunc(GL_EQUAL, 1, 1);
-
+	// render the rest of the skybox, apart from the area inside the portal
+	glStencilFunc(GL_GREATER, 1, 1);
 	skybox->render(sceneCamera->getPosition());
 
+	// now we want to fill in the depth information of the portal, since everything behind it has already been drawn
+	glDisable(GL_STENCIL_TEST);
+	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+	RenderHelper::drawMesh(portalScreen);
+	glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+	glDisable(GL_STENCIL_TEST);
+
+	// render everything on the other side of the portal
 	{
 		Transformation t{ Vector3::zero, Vector3::zero, {10, 1, 10} };
 		RenderHelper::drawMesh(groundPlane);
 	}
-
-	glDisable(GL_STENCIL_TEST);
+	{
+		Transformation t({ 0, 1.0f, -3.0f });
+		RenderHelper::drawSphere(1.0f);
+	}
 
 	// render everything on both sides of the portal
 	RenderHelper::drawMesh(portal);
